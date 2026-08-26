73fe1017f7b950a35dfd19c13ddaa59299058062
jianweilor <65474516+jianweilor@users.noreply.github.com>
2026-08-26 15:12:15 +0800

    Fix formatting in step2_greeting function

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def step1_greeting() -> str:
 def step2_greeting() -> str:
     return (
         "What are the requirements for your vehicle(s)? "
-        "You can describe your needs (e.g. *'SUV with at least 7 seats'*, *'sedans that do 0-100 in under 8 seconds'* )"
+        "You can describe your needs (e.g. *'SUV with at least 7 seats'*, *'sedans that do 0-100 in under 8 seconds'*) "
         "and on our **Recommender Agent** will assist you!"
     )
 
